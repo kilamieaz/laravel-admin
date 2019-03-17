6de1e18cf75b281669c30c0e8bcb226ea74a8551
kilamieaz <im.sultaan05@gmail.com>
2019-03-17 21:27:15 +0700

    bismillah

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        $request->user()->authorizeRoles(['admin', 'buyer']);
+        return view('home');
+    }
+
+    public function adminView(Request $request)
+    {
+        $request->user()->authorizeRoles('admin');
+        return view('admin');
+    }
+}
